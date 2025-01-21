0cdf1fc7f0297cff6e9bae8c3d5b6d7b6e5a1e02
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-21 22:07:28 +0530

    Add files via upload

---
+++ multi.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2,multi;
+	printf("enter a num1:");
+	scanf("%d",&num1);
+	printf("enter num2:");
+	scanf("%d",&num2);
+	multi=num1*num2;
+	printf("multiplication:%d",multi);
+	return 0;
+	}
