3b22931634eba10c21a2d84f7efcab623308ef2d
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-21 22:09:58 +0530

    Add files via upload

---
+++ relational.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter first num1:");
+	scanf("%d",&a);
+	printf("enter second num2:");
+	scanf("%d",&b);
+	printf("\na==b:%d",a==b);
+	printf("\na!=b:%d",a!=b);
+	printf("\na<b:%d",a<b);
+	printf("\na>b:%d",a>b);
+	printf("\na<=:%d",a<=b);
+	printf("\na>=b:%d",a>=b);
+	return 0;
+	
+	
+}
