81179e88af0fa2a26816cdcc193b1a67d513d50d
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-21 22:06:47 +0530

    Add files via upload

---
+++ div.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int no1,no2,div;
+	printf("enter a first no=");
+	scanf("%d",&no1);
+	printf("enter a second no=");
+	scanf("%d",&no2);
+	div=no1/no2;
+	printf("division =%d",div);
+	return 0;
+	}
