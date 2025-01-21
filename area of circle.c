857353ef807affdcec6dd8f7e9fecfb45731a360
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-21 21:56:44 +0530

    Add files via upload

---
+++ area of circle.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int radius,area;
+	printf("enter a value of radius=");
+	scanf("%d",&radius);
+	area=3.14*radius*radius;
+	printf("area of circle is=%d",area);
+	return 0;
+}
