c73fe09ba49677e2056dd24d1e762696cd656cd4
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-21 21:56:14 +0530

    Add files via upload

---
+++ area of rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int length,width,area;
+	printf("enter a length of rectangle=");
+	scanf("%d",&length);
+	printf("enter a width of rectangle=");
+	scanf("%d",&width);
+	area=length*width;
+	printf("area of rectangle=%d",area);
+	return 0;
+	
+}
