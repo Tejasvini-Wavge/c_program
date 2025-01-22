c002c9a01f0c586c6deea1645c0eedcfb2b9bd61
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-22 20:33:13 +0530

    Add files via upload

---
+++ logicalOperator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2;
+	printf("enter first number:");
+	scanf("%d",&num1);
+	printf("enter second number:");
+	scanf("%d",&num2);
+	printf("\n (num1>num2) && (num2>num1)=%d",(num1>num2) && (num2>num1));
+	printf("\n (num1>num2) || (num2>num1)=%d",(num1>num2) || (num2>num1));
+	printf("\n !(num1>num2) =%d",!(num1>num2));
+	printf("\n !(num1>num2) =%d",!(num1>num2));
+	return 0;
+	
+
+
+	
+	
+}
