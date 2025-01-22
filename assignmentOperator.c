8ce50a93de201e522ae6a0ebf161830e4da8c6a7
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-22 20:34:18 +0530

    Add files via upload

---
+++ assignmentOperator.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Enter first number:");
+scanf("%d",&a);
+printf("Enter second number:");
+scanf("%d",&b);
+printf("\nAddition=%d,a+=b");
+printf("\nsubstraction=%d,a-=b");
+printf("\nMultiplication=%d,a*=b");
+printf("\nDivision=%d,a/=b");
+printf("\nModulus=%d,a%=b");
+return 0;
+}
+
+
+
