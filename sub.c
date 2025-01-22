428f004f6dd5ffda4a94e9f2c3301cc0bfcf31fe
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-22 20:36:51 +0530

    Add files via upload

---
+++ sub.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int no1,no2,sub;
+	printf("enter no1:");
+	scanf("%d",&no1);
+	printf("enter no2:");
+	scanf("%d",&no2);
+	sub=no1-no2;
+	printf("sub=%d",sub);
+	return 0;
+	}
