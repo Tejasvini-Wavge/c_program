f746a7eecdb68f5f06855821904016712ca3fe5a
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-23 20:42:44 +0530

    Add files via upload

---
+++ decrement_Operator.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter number:");
+	scanf("%d",&a);
+	printf("\n a++:%d",a++);
+	printf("\n ++a:%d",++a);
+	printf("\n%d",a);
+	printf("\n --a=%d",--a);
+	printf("\n a--=%d",a--);
+	printf("\n%d",a);
+	return 0;
+	
+}
