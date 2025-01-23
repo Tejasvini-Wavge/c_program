1db7f55ef254c01bb694cb114d36f52a31d914ec
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-23 20:43:08 +0530

    Add files via upload

---
+++ increment_Oprator.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int no1;
+	printf("enter number:");
+	scanf("%d",&no1);
+	printf("\n no1++:%d",no1++);
+	printf("\n ++no1:%d",++no1);
+	printf("\n%d",no1);
+	return 0;
+}
