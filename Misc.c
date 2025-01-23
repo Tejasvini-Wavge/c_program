5a9fb2b3ed03be0486c655f4c3363b104b8a7894
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-23 20:43:55 +0530

    Add files via upload

---
+++ Misc.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	float b;
+	char c;
+	double d;
+	printf("\n%d Bytes",sizeof(a));
+	printf("\n%d Bytes",sizeof(b));
+	printf("\n%d Bytes",sizeof(c));
+	printf("\n%d Bytes",sizeof(d));
+
+}
