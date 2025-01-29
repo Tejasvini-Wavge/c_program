78956685792504f6e0db2605600021002700da58
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-29 22:35:16 +0530

    Add files via upload

---
+++ formatSpecifier.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int a=20;
+	int f=0100;
+	float b=20.30;
+	char c='s';
+	int d=0xA;
+	int e=-10;
+     printf("\n the value a is=%d",a);
+	 printf("\nfloat point value is=%f",b);
+	 printf("\nvalue of c is=%c",c);
+	 printf("\n octal value is =%o",f);
+	 printf("\n the value e is =%u",e);
+	 printf("\nHexadecimal value of d is=%x",d);
+	 printf("\nHexadecimal value of d is=%X",d);
+	 printf("\nAddress value of c in hexadecimal form is%p",&c);
+	 printf("\nfloat value of d is=%g",b);
+	 printf("\n%X",d);
+	 
+}
