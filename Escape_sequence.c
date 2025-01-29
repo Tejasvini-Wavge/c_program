378e247527c9912247cf0a4088acf7172a0eb2e9
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-29 22:34:44 +0530

    Add files via upload

---
+++ Escape_sequence.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int number=50;
+	printf("\nnew line");
+	printf("\a alaram or beap");
+	printf("\b backspace");
+	printf("\f form feed");
+	printf("\r carriage return");
+	printf("\t tab horizontal");
+	printf("\v tab vertical");
+	printf("\\ backlash");
+	printf("\'hello\' single quote");
+	printf("\"hello\" double quote");
+	printf("\?hello Question mark");
+	printf("\nnn octal number");
+	printf("\0 Null");
+	
+}
