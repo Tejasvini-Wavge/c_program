2289c783b3f3c9c3b40335624ffeca8684bbde54
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-01-29 21:31:25 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int X=65;
+	int x=97;
+	char a='A';
+	char A='a';
+	char c='@';
+	printf("\n%c",X);
+	printf("\n%c",x);
+	printf("\n%d",a);
+	printf("\n%d",A);
+	printf("\n%d",c);
+	return 0;
+}
