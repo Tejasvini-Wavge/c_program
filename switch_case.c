f66484346abe93a5bd680d23adf8704f0c80d076
Tejasvini-Wavge13 <156306939+Tejasvini-Wavge@users.noreply.github.com>
2025-02-04 22:15:43 +0530

    Add files via upload

---
+++ switch_case.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int num
+	printf("enter a number");
+	scanf("%d",&num);
+	switch(number){
+		case(num<0):
+		printf("it is a negative number");
+		break;
+		case(num>0):
+			printf("it is a positive number");
+			break;
+			default
+	}
+}
